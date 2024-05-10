fffbbd16a5dc0e70e066f274ed0163cd7852b1a7
Greg Salisbury <gregsalisbury25@gmail.com>
2024-05-09 21:34:26 -0400

    Creating proper project structure

---
+++ src/AirportSim/Flight.java
@@ -0,0 +1,156 @@
+package AirportSim;
+
+import java.util.*;
+
+public class Flight
+{//begin Flight class
+
+    //integer variables which store different times
+    private int departureTime;
+
+    public int getDepartureTime() {return departureTime;}
+
+    public void setDepartureTime(int departureTime) {this.departureTime = departureTime;}
+    private int departureTimeHour;
+
+    public void setDepartureTimeHour(int departureTimeHour) {
+        this.departureTimeHour = departureTimeHour;
+    }
+
+    private int departureTimeMin;
+
+    public void setDepartureTimeMin(int departureTimeMin) {this.departureTimeMin = departureTimeMin;}
+
+    private int landingTime;
+
+    public void setLandingTime(int landingTime) {this.landingTime = landingTime;}
+
+    private int landingTimeHour;
+
+    public void setLandingTimeHour(int landingTimeHour) {this.landingTimeHour = landingTimeHour;}
+
+    private int landingTimeMin;
+
+    public void setLandingTimeMin(int landingTimeMin) {this.landingTimeMin = landingTimeMin;}
+
+    //int variable number representing the flight number
+    private int number;
+
+    public int getNumber() {return number;}
+
+    public void setNumber(int number) {this.number = number;}
+
+    //String object destination representing the destination of the flight
+
+    private String destination;
+
+    public String getDestination() {return destination;}
+
+    public void setDestination(String destination) {this.destination = destination;}
+
+    private String originAirport;
+
+    public void setOriginAirport(String originAirport) {this.originAirport = originAirport;}
+
+    //Plane object being used for Flight parameterized constructor
+
+    private Plane plane;
+
+    public Plane getPlane() {return plane;}
+
+    public void setPlane(Plane plane) {this.plane = plane;}
+
+    private Gate gate;
+
+    public Gate getGate() {return gate;}
+
+    public void setGate(Gate gate)
+    {
+
+        this.gate = gate;
+
+        gate.gs = Gate.gateStatus.OCCUPIED;
+
+    }
+
+    private final flightStatus fs;
+
+    private final ArrayList<Passenger> paxWithTickets = new ArrayList<>();
+
+    public ArrayList<Passenger> getPaxWithTickets() {return paxWithTickets;}
+
+    //Flight parameterized constructor, assigning a plane, a destination, a number, and the departure time of each flight
+
+    public Flight(Plane plane, String destination, String originAirport, int number, int departureTimeHour, int departureTimeMin, int departureTime, Gate gate)
+    {//begin Flight parameterized constructor
+
+        setPlane(plane);
+        setDestination(destination);
+        setOriginAirport(originAirport);
+        setNumber(number);
+        setDepartureTimeHour(departureTimeHour);
+        setDepartureTimeMin(departureTimeMin);
+        setDepartureTime(departureTime);
+        setGate(gate);
+        gate.addPlane(plane);
+        fs = flightStatus.ONTIME;
+
+    }//end Flight parameterized constructor
+
+    public boolean isSoldOut()
+    {//begin isSoldOut
+
+        return paxWithTickets.size() >= plane.getPassengerCapacity();
+
+    }//end isSoldOut
+
+
+    //flight status enum to assign a flight status depending on the events of the flight
+
+    enum flightStatus
+    {//begin flightStatus enum
+
+        // EARLY,
+        ONTIME,
+        // DELAYED,
+        // CANCELLED
+
+    }//end flightStatus enum
+
+    public String toString()
+    {
+
+        return ("\nFlight Number: " + number + "\nOrigin: " + originAirport + "\nDestination: " + destination +
+                "\n" + formatTime("Departure", departureTimeHour, departureTimeMin, departureTime) +
+                "\n" + formatTime("Landing", landingTimeHour, landingTimeMin, landingTime) +
+                "\nGate: " + gate.getName() +
+                "\nFlight Status: " + fs);
+
+    }
+    
+    public String formatTime(String status, int hour, int min, int time)
+    {//begin printTime
+
+        String suffix, zero;
+
+        if (time > 719 && time < 1440)
+            suffix = " PM";
+        else
+            suffix = " AM";
+
+        if (min < 10)
+            zero = "0";
+        else
+            zero = "";
+
+        if (time < 60)
+            hour = 12;
+
+        if (hour > 12)
+            hour -= 12;
+
+        return status + " time: " + hour + ":" + zero + min + suffix;
+
+    }//end printTime
+
+}//end flight
---
+++ src/AirportSim/Passenger.java
@@ -0,0 +1,190 @@
+package AirportSim;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Passenger extends Person
+{//begin Passenger class
+
+    Random random = new Random();
+
+    //Integer ArrayList storing the weights of passengers' bags
+
+    private ArrayList<Integer> bags = new ArrayList<>();
+
+    //generatePassengerBags to assign a Passenger object bags
+
+    public ArrayList<Integer> generatePassengerBags()
+    {//begin getPassengerBags
+
+        ArrayList<Integer> passengerBags = new ArrayList<>();
+
+        //assigning random int between 1 and 3 inclusive, determining the amount of times the inner loop will run, and the number of bags that will be created
+
+        int numOfBags = random.nextInt(3) + 1;
+
+        //inner loop, "j" representing the creation of a passenger's bag, and the element of that bag in the bags ArrayList
+        //bagWeight represents the weight of a passenger's bag in pounds, 50 being the upper limit. All bags are between 20 and 50 pounds inclusive at the moment
+
+        for (int i = 0; i < numOfBags; i++)
+        {//begin for loop to generate bag weights
+
+            int bagWeight;
+
+            bagWeight = random.nextInt(31)+20;
+
+            passengerBags.add(bagWeight);
+
+        }//end for loop to generate bag weights
+
+        return passengerBags;
+
+    }//end getPassengerBags
+
+    //placeholder int variable for Flight Numbers before they are passed in during passenger construction
+
+    private final int flightNumber;
+
+    public int getFlightNumber() {return flightNumber;}
+
+    private final String destination;
+
+    private int curbToCheckIn, checkInToSecurity, securityToGate, gateToPlane;
+
+    public int getCurbToCheckIn() {return curbToCheckIn;}
+
+    public int getCheckInToSecurity() {return checkInToSecurity;}
+
+    public int getSecurityToGate() {return securityToGate;}
+
+    public int getGateToPlane() {return gateToPlane;}
+
+    public boolean isAtGate() {return at == airportTravel.AT_GATE;}
+
+    enum airportTravel
+    {//begin airportTravel enum
+
+        DROPPED_OFF,
+        // CHECKING_IN,
+        CHECKED_IN,
+        // AT_SECURITY,
+        THROUGH_SECURITY,
+        // WALKING_TO_GATE,
+        AT_GATE,
+        // BOARDING_PLANE
+
+    }//end airportTravel enum
+
+    airportTravel at;
+
+    private final Ticket ticket;
+
+
+    //Passenger parameterized constructor, assigning each passenger a flight number, an id, and 1-3 bags inclusive
+
+    public Passenger(int flightNumber, String destination, String id)
+    {//begin Passenger parameterized constructor
+
+        this.flightNumber = flightNumber;
+        this.destination = destination;
+        setId(id);
+        bags = generatePassengerBags();
+        commuteThroughAirport();
+        ticket = new Ticket();
+
+        curbToCheckIn = getCurbToCheckIn();
+        checkInToSecurity = getCheckInToSecurity();
+        securityToGate = getSecurityToGate();
+        gateToPlane = getGateToPlane();
+
+        at = airportTravel.DROPPED_OFF;
+
+
+    }//end Passenger parameterized constructor
+
+    //printPassenger to print information about a passenger
+
+    public String toString()
+    {//begin Passenger toString
+
+        String paxInfo = "\nPassenger Flight Number: " + flightNumber + "\nPassenger ID: " + getId() +
+        "\nTicket Number: " + ticket.ticketNum + "\nPassenger destination: " + destination;
+
+        StringBuilder paxBags = new StringBuilder();
+
+        for(int i = 0; i < bags.size(); i++)
+        {//begin for loop to print all passenger's bag weights
+
+            paxBags.append("\nBag ").append(i + 1).append(" Weight: ").append(bags.get(i)).append(" pounds");
+
+        }//end for loop to print all passenger's bag weights
+
+        return paxInfo + paxBags;
+
+    }//end Passenger toString()
+
+    /* commuteThroughAirport generates durations for each stage of
+    a passenger getting from the curb to the gate of their airport */
+
+    public void commuteThroughAirport()
+    {//begin commuteThroughAirport
+
+        curbToCheckIn = random.nextInt(11)+5;
+        checkInToSecurity = random.nextInt(6)+5;
+        securityToGate = random.nextInt(31)+15;
+        gateToPlane = random.nextInt(11)+5;
+
+    }//end commuteThroughAirport
+
+    /* movePassenger to get passengers to their gate through decrementing
+    the above variables and switching enums when each field equals 0. The method
+    is called for a passenger while they have not made it to their gate. */
+
+    public void movePassenger()
+    {//begin movePassenger
+
+        if(at.equals(airportTravel.DROPPED_OFF))
+        {//begin if passenger has been dropped off but not checked in
+
+            curbToCheckIn--;
+
+            if(curbToCheckIn == 0)
+            {//begin if passenger has made it to check in
+
+                at = airportTravel.CHECKED_IN;
+
+            }//end if passenger has made it to check in
+
+        }//end if passenger has been dropped off but not checked in
+
+        if(at.equals(airportTravel.CHECKED_IN))
+        {//begin if passenger has checked in but not been through security
+
+            checkInToSecurity--;
+
+            if(checkInToSecurity == 0)
+            {//begin if passenger gets through security
+
+                at = airportTravel.THROUGH_SECURITY;
+
+            }//end if passenger gets through security
+
+        }//end if passenger has checked in but not been through security
+
+        if(at.equals(airportTravel.THROUGH_SECURITY))
+        {//begin if passenger is through security but not gotten to gate
+
+            securityToGate--;
+
+            if(securityToGate == 0)
+            {
+
+                at = airportTravel.AT_GATE;
+
+            }
+
+        }//end if passenger is through security but not gotten to gate
+
+    }//end movePassenger
+
+}//end Passenger class
