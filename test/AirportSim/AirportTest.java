ad7819adc50f229379a12dde7c5657488089de12
gregrs400 <gregsalisbury25@gmail.com>
2024-05-18 14:18:44 -0400

    Removed unused import statements

--- test/AirportSim/AirportTest.java
+++ test/AirportSim/AirportTest.java
@@ -1,7 +1,5 @@
 package AirportSim;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
