3d5fa233cac49092d2c1cc1c78637cfcd80cecac
gregrs400 <gregsalisbury25@gmail.com>
2024-06-18 19:50:37 -0400

    modifying flight generation and adding list for planes that can be assigned to a flight

--- src/AirportSim/Airline.java
+++ src/AirportSim/Airline.java
@@ -3,15 +3,20 @@ package AirportSim;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Random;
+import AirportSim.Time;
 
 public class Airline
 {//begin Airline class
 
+    private final Random random = new Random();
+
+    private final Time time = new Time();
+
     //String ArrayList of destinations offered by an airline from this airport
 
-    private ArrayList<String> destinations = new ArrayList<>();
+    private ArrayList<Airport> destinations = new ArrayList<>();
 
-    public void setDestinations(ArrayList<String> destinations) {this.destinations = destinations;}
+    public void setDestinations(ArrayList<Airport> destinations) {this.destinations = destinations;}
 
     //Integer ArrayList of Flight Numbers of flights created by the airline
 
@@ -37,15 +42,24 @@ public class Airline
 
     public HashMap<Integer, Flight> getFlightNumToFlight() {return flightNumToFlight;}
 
+    private final ArrayList<Plane> availablePlanes = new ArrayList<>();
+
     //Airline parameterized constructor
 
-    public Airline(ArrayList<String> destinations, ArrayList<Integer> flightNumbers)
+    public Airline(ArrayList<Airport> destinations)
     {//begin Airline parameterized constructor
 
         setDestinations(destinations);
-        setFlightNumbers(flightNumbers);
 
     }//end Airline parameterized constructor
+
+    public Airline(Airline anotherAirline)
+    {
+
+        this(anotherAirline.destinations);
+        this.flightNumbers = anotherAirline.flightNumbers;
+
+    }
 
     //addPlane to add a plane to an airline's fleet
 
@@ -53,17 +67,62 @@ public class Airline
     {//begin addPlane
 
         airlineFleet.add(plane);
+        availablePlanes.add(plane);
 
     }//end addPlane
 
     //generateFlight to create a flight for an airline, assigning each flight a plane, a destination, a flight number, and a departure time
 
-    public Flight generateFlight(Plane plane, String origin, int departHour, int departMin, int departTime, Gate gate)
+
+    public Flight generateFlight()
+    {
+
+        Airport origin, destination;
+
+        int flightNumber;
+
+        //do-while loop to ensure each flight number is unique
+
+        do
+        {//begin do-while loop
+
+            flightNumber = random.nextInt(9999)+1;
+
+        }while(flightNumbers.contains(flightNumber)); //end do-while loop
+
+        flightNumbers.add(flightNumber);
+
+        Plane plane = availablePlanes.get(0);
+        availablePlanes.remove(0);
+
+        int originIndex = random.nextInt(destinations.size());
+
+        int destinationIndex = random.nextInt(destinations.size());
+
+        origin = destinations.get(originIndex);
+
+        if (destinationIndex == originIndex)
+        {
+            while (destinationIndex == originIndex) {
+                destinationIndex = random.nextInt(destinations.size());
+            }
+        }
+
+        destination = destinations.get(destinationIndex);
+
+        Flight flight = new Flight(plane, destination, origin, flightNumber);
+
+        flights.add(flight);
+
+        flightNumToFlight.put(flight.getNumber(), flight);
+
+        return flight;
+
+    }
+    public Flight generateFlight(Plane plane, Airport origin, int departHour, int departMin, int departTime, Gate gate)
     {//begin generateFlight
 
-        Random random = new Random();
-
-        String destination;
+        Airport destination;
 
         int flightNumber;
 
@@ -87,6 +146,36 @@ public class Airline
         flightNumToFlight.put(flight.getNumber(), flight);
 
         return flight;
+
+    }
+
+    public void generateFlights()
+    {
+
+        for (Plane plane : airlineFleet)
+        {
+
+            int originIndex = random.nextInt(airlineFleet.size()-1);
+            int destIndex = random.nextInt(airlineFleet.size()-1);
+
+            if (destIndex == originIndex)
+            {
+
+                while (destIndex == originIndex)
+                {
+
+                    destIndex = random.nextInt(airlineFleet.size()-1);
+
+                }
+
+            }
+
+            Airport origin = destinations.get(originIndex);
+            Airport destination = destinations.get(destIndex);
+
+
+
+        }
 
     }
 
@@ -114,7 +203,7 @@ public class Airline
         return getFlightNumToFlight().get(passenger.getFlightNumber());
     }
 
-    public void removeFromDestinations(String destination)
+    public void removeFromDestinations(Airport destination)
     {//begin removeFromDestinations
 
         destinations.remove(destination);
