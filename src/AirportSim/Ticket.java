7a87c05ec313a0737af948bb21d9cb3f91b533e7
gregrs400 <gregsalisbury25@gmail.com>
2024-06-18 19:53:56 -0400

    Ticket: copy constructor and more useful data

--- src/AirportSim/Ticket.java
+++ src/AirportSim/Ticket.java
@@ -8,8 +8,18 @@ public class Ticket {
 
     String ticketNum = "";
 
-    public Ticket() {
+    private Flight flight;
+
+    public Ticket(Flight flight) {
         ticketNum = formatter();
+        this.flight = flight;
+    }
+
+    public Ticket(Ticket anotherTicket)
+    {
+
+        this(anotherTicket.getFlight());
+
     }
 
     public String formatter() {
@@ -17,6 +27,13 @@ public class Ticket {
         DecimalFormat df = new DecimalFormat("#");
         df.setMaximumFractionDigits(0);
         return "1" + df.format(Math.random() * 1000000000000L);
+    }
+
+    public Flight getFlight()
+    {
+
+        return flight;
+
     }
 
 }
