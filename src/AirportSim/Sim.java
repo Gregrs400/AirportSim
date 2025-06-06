6f4423bb6e193a3857ede3f59ff2ef309f6d6e4e
Greg Salisbury <gregsalisbury25@gmail.com>
2025-06-06 06:28:47 -0400

    Removed seat testing code

--- src/AirportSim/Sim.java
+++ src/AirportSim/Sim.java
@@ -115,18 +115,6 @@ public class Sim   // upper level class to enclose all objects
 
         }
 
-        Plane testPlane = airlineOne.getAirlineFleet().get(1);
-        Flight flight1 = airlineOne.generateFlight(testPlane, airports.get(2), airports.get(5));
-
-        Passenger passenger1 = new Passenger(flight1, "pax1");
-        Passenger passenger2 = new Passenger(flight1, "pax2");
-
-        passenger1.reserveRandomSeat();
-        passenger2.reserveRandomSeat();
-        System.out.println(testPlane.getSeats());
-        System.out.println("passenger1 seat on ticket: " + passenger1.getTicket().getSeatCode());
-        System.out.println("passenger2 seat on ticket: " + passenger2.getTicket().getSeatCode());
-
     }
 
     public static void addToMovingObjects(int startTime, int duration, MovingObject object)
