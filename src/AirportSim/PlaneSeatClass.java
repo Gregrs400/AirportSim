62201854de8e4ee62924c5d98c658c22cb473181
Greg Salisbury <gregsalisbury25@gmail.com>
2025-06-28 11:42:51 -0400

    Made amenities final

--- src/AirportSim/PlaneSeatClass.java
+++ src/AirportSim/PlaneSeatClass.java
@@ -6,7 +6,7 @@ public class PlaneSeatClass
 {
 
     private final String className;
-    private ArrayList<String> amenities;
+    private final ArrayList<String> amenities;
     private final String classCode;
 
     public PlaneSeatClass(String className, ArrayList<String> amenities, String classCode)
