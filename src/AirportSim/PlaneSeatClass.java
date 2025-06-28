97d6a397963f63fb1e13e5bfbf2d63890949ceb4
Greg Salisbury <gregsalisbury25@gmail.com>
2025-06-28 11:41:11 -0400

    Made className final

--- src/AirportSim/PlaneSeatClass.java
+++ src/AirportSim/PlaneSeatClass.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 public class PlaneSeatClass
 {
 
-    private String className;
+    private final String className;
     private ArrayList<String> amenities = new ArrayList<>();
     private String classCode;
 
